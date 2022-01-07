935f666f9a6c807a533b094d155559d83ca47668
Robert-Jan Peters <rjf.peters@hotmail.com>
2022-01-07 17:55:24 +0100

    feature(autonomous-grid-manager) - add feign ActuatorHealthResponse

---
+++ mod1-agm-service/src/main/java/tld/yggdrasill/services/agm/client/contingency/model/ActuatorHealthResponse.java
@@ -0,0 +1,12 @@
+package tld.yggdrasill.services.agm.client.contingency.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActuatorHealthResponse {
+  private String status;
+}
